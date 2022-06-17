56db63303de7ca61066839fd7ecfdf7eb7649852
mansipalkar19 <38622985+mansipalkar19@users.noreply.github.com>
2022-06-17 23:49:12 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -73,33 +73,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-/*$db['default'] = array(
+$db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'school',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);*/
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'sql111.epizy.com',
-	'username' => 'epiz_31972245',
-	'password' => '3ZaCWDaa84RKtNs',
-	'database' => 'epiz_31972245_school',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
